5392816cd451bee3f8bde0209d617137f4fb41b2
Prashansa Rai <65287608+prashansarai@users.noreply.github.com>
2023-07-30 08:25:15 +0530

    edit btn added

--- dom-3_Revision.js
+++ dom-3_Revision.js
@@ -18,25 +18,41 @@
 // localStorage.setItem('myObj', myObj_deserialised);
 // console.log(localStorage.getItem('myObj'));
 var form = document.getElementById('addForm');
-var nameVal = document.getElementById('name');
-var emailVal= document.getElementById('email');
-var phnVal= document.getElementById('phnno');
-form.addEventListener('submit',showItems);
+
 form.addEventListener('submit',showLists);
-function showItems(e){
-    e.preventDefault();
-    let myObj = {'Name': nameVal.value, 'E-Mail': emailVal.value, 'Phn-No': phnVal.value};
-    let myObj_serialised = JSON.stringify(myObj);
-    localStorage.setItem(emailVal.value,myObj_serialised);
-    console.log(localStorage.getItem(emailVal.value));
-}
+
 function showLists(e){
     e.preventDefault();
     var li = document.createElement('li');
     var ul = document.getElementById('items');
-    li.className = "listItems";
-    li.appendChild(document.createTextNode(nameVal.value +' - '+emailVal.value +' - '+phnVal.value));
+    var dltBtn = document.createElement('button');
+    var edtBtn = document.createElement('button');
+    dltBtn.className = 'btn dlt';
+    edtBtn.className = 'btn edt';
+    dltBtn.appendChild(document.createTextNode('Delete'));
+    edtBtn.appendChild(document.createTextNode('Edit'));
+    dltBtn.style.backgroundColor= '#f40000';
+    edtBtn.style.backgroundColor= '#f4f4f4'
+    li.className= 'listItems';
+    var nameVal = e.target.username.value;
+    var emailVal= e.target.email.value;
+    var phnVal= e.target.phnno.value;
+    var myObj = {nameVal,emailVal,phnVal};
+    var myObj_serialised = JSON.stringify(myObj);
+    localStorage.setItem(myObj.emailVal,myObj_serialised);
+    li.appendChild(document.createTextNode(myObj.nameVal +' - '+ myObj.emailVal +' - '+ myObj.phnVal));
+    li.appendChild(dltBtn);
+    li.appendChild(edtBtn);
     ul.appendChild(li);
+    dltBtn.onclick = () =>{
+        localStorage.removeItem(myObj.emailVal);
+        ul.removeChild(li);
+    }
+    edtBtn.onclick = () =>{
+        document.getElementById('username').value = myObj.nameVal;
+        document.getElementById('email').value  = myObj.emailVal;
+        document.getElementById('phnno').value  = myObj.phnVal;
+        localStorage.removeItem(myObj.emailVal);
+        ul.removeChild(li);
+    }
 }
-
-
